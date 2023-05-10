f0e2185de4238c0a59de98ecf2a61630fc664347
Micah Johnson <mj2carlsbad@gmail.com>
2023-05-09 19:22:16 -0700

    move js to php

--- index.php
+++ index.php
@@ -8,6 +8,27 @@
     <script src="todo.js"></script>
 </head>
 
+<script>
+    window.addEventListener("load", function () {
+    let items = document.getElementById("items");
+    let newItemBtn = document.getElementById("newItemBtn");
+
+    newItemBtn.addEventListener("click", function () {
+        let newItem = document.createElement("div");
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        let desc = document.createElement("input");
+        desc.type = "text";
+
+        newItem.appendChild(checkbox);
+        newItem.appendChild(desc);
+        items.appendChild(newItem);
+    })
+})
+</script>
+
 <body>
     <h1>My List</h1>
     <div id="items"></div>
